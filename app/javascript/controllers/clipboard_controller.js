3373a984b0fe08e30aa3982db849e545d483dddb
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2021-12-20 22:36:57 +0100

    added back link copy

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -2,10 +2,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "source" ]
+  static targets = [ "text" ]
   
   copy() {
-    this.sourceTarget.select()
-    document.execCommand("copy")
+    navigator.clipboard.writeText(window.location.href);
+    const element = this.textTarget
+    element.classList.remove("hidden")
+    setInterval(function () { element.classList.add("hidden")}, 1500);
   }
 }
